839c64de9b1b000e4321915eeebcd4c161010c3a
Ujjwal Sharma <usharma1998@gmail.com>
2017-05-26 00:58:42 +0530

    Segregated the irc service

--- bot.js
+++ bot.js
@@ -1,11 +1,9 @@
-const irc = require('irc');
-
 const config = require('./config');
 
-const bot = new irc.Client(config.server, config.name, {
-  channels: config.channels
-});
+const ircService = require('./services/irc');
 
-bot.addListener('join', (channel, who) => {
-  bot.say(channel, `Welcome to Publiclab, ${who}!`);
+const bot = ircService.newBot(config.server, config.name, config.channels);
+
+ircService.addJoinHandler(bot, (bot, channel, username) => {
+  bot.say(channel, `Welcome to Publiclab, ${username}!`);
 });
