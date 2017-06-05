b1fe2ee013ac75dd42fd97694828a8e2a8da0062
Ujjwal Sharma <usharma1998@gmail.com>
2017-06-05 23:40:14 +0530

    Add cli interface (resolves #9)

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 const config = require('./config');
 
-const IrcClient = require('./interfaces/irc');
 const Chatbot = require('./services/chatbot');
 
-const ircClient = new IrcClient(config.server, config.name, config.channels);
-const chatbot = new Chatbot(config.name, ircClient);
+if (process.env.TEST) {
+  const CliClient = require('./interfaces/cli');
+  var client = new CliClient(config.name);
+  console.log('Bot is running in testing mode.');
+} else {
+  const IrcClient = require('./interfaces/irc');
+  var client = new IrcClient(config.server, config.name, config.channels);
+}
+
+const chatbot = new Chatbot(config.name, client);
 
 chatbot.addListeners();
---
+++ interfaces/cli.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+
+class CliClient {
+  constructor(nick) {
+    this.nick = nick;
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
+  }
+
+  addJoinHandler() {
+    // Ain't nobody joinin' you on the terminal
+  }
+
+  sendMessage(channel, message) {
+    console.log(`${channel} => ${message}`);
+  }
+
+  addMessageHandler(actions) {
+    this.rl.on('line', (line) => {
+      actions(process.env.USER, this.nick, line);
+    });
+  }
+}
+
+module.exports = CliClient;
