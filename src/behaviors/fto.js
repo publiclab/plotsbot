47fc56df09f48f6a1ee7dc1f091de02b1356feef
Ujjwal Sharma <usharma1998@gmail.com>
2017-06-30 16:27:55 +0530

    Refine FTO behavior to accept names of multiple repositories

--- src/behaviors/fto.js
+++ src/behaviors/fto.js
@@ -3,16 +3,30 @@ const Behavior = require('../models/behavior');
 
 const github = new Github();
 
-const ftoAction = () => {
+function ftoRepo(repo) {
   return github.issues.getForRepo({
     owner: 'publiclab',
-    repo: 'plots2',
+    repo,
     labels: 'first-timers-only'
   }).then(data => {
     return data.data.reduce((acc, issue) => {
       return acc + `\n${issue.number} => ${issue.title}`;
-    }, '');
+    }, `publiclab/${repo}`);
+  }).catch(err => {
+    if (err.status === 'Not Found') {
+      return `publiclab/${repo} is not a valid repository.`;
+    } else {
+      throw err;
+    }
   });
+}
+
+const ftoAction = (botNick, options) => {
+  if (options.length > 0) {
+    return Promise.all(options.map(ftoRepo)).then(repos => repos.join('\n\n'));
+  } else {
+    return 'You need to mention the name of a repository.';
+  }
 };
 
 module.exports = new Behavior('message', ftoAction, 'fto');
