267ec1815fe50ff729eda006049a4da48bdf923a
Ujjwal Sharma <usharma1998@gmail.com>
2017-07-02 03:21:52 +0530

    Use the newer shinier model

--- src/behaviors/fto.js
+++ src/behaviors/fto.js
@@ -1,32 +1,31 @@
-const Github = require('github');
 const Behavior = require('../models/behavior');
 
-const github = new Github();
-
-function ftoRepo(repo) {
-  return github.issues.getForRepo({
-    owner: 'publiclab',
-    repo,
-    labels: 'first-timers-only'
-  }).then(data => {
-    return data.data.reduce((acc, issue) => {
-      return acc + `\n${issue.number} => ${issue.title}`;
-    }, `publiclab/${repo}`);
-  }).catch(err => {
-    if (err.status === 'Not Found') {
-      return `publiclab/${repo} is not a valid repository.`;
-    } else {
-      throw err;
-    }
-  });
-}
-
-const ftoAction = (botNick, options) => {
-  if (options.length > 0) {
-    return Promise.all(options.map(ftoRepo)).then(repos => repos.join('\n\n'));
-  } else {
-    return 'You need to mention the name of a repository.';
+module.exports = ({ github }) => {
+  function ftoRepo(repo) {
+    return github.issues.getForRepo({
+      owner: 'publiclab',
+      repo,
+      labels: 'first-timers-only'
+    }).then(data => {
+      return data.data.reduce((acc, issue) => {
+        return acc + `\n${issue.number} => ${issue.title}`;
+      }, `publiclab/${repo}`);
+    }).catch(err => {
+      if (err.status === 'Not Found') {
+        return `publiclab/${repo} is not a valid repository.`;
+      } else {
+        throw err;
+      }
+    });
   }
-};
 
-module.exports = new Behavior('message', ftoAction, 'fto');
+  const ftoAction = (botNick, options) => {
+    if (options.length > 0) {
+      return Promise.all(options.map(ftoRepo)).then(repos => repos.join('\n\n'));
+    } else {
+      return 'You need to mention the name of a repository.';
+    }
+  };
+
+  return new Behavior('message', ftoAction, 'fto');
+};
--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
+const Github = require('github');
 
 const Behaviors = require('./behaviors');
 
+const state = {
+  github: new Github()
+};
+
 const greetBehavior = require('./behaviors/greet');
 const helpBehavior = require('./behaviors/help').helpBehavior;
-const ftoBehavior = require('./behaviors/fto');
+const ftoBehavior = require('./behaviors/fto')(state);
 
 // Read file synchronously because we'd need this object in later steps anyway.
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 let client;
-
 if (process.env.TEST) {
   const CliClient = require('./interfaces/cli');
   client = new CliClient(config.name);
