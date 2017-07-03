6aca704650f83383b9f7841b7629c8f00069b75f
Ujjwal Sharma <usharma1998@gmail.com>
2017-07-03 22:57:22 +0530

    Add short-term memory to the greet behavior

--- src/behaviors/greet.js
+++ src/behaviors/greet.js
@@ -1,7 +1,13 @@
 const Behavior = require('../models/behavior');
+const utils = require('../utils');
+
+let greeted = [];
 
 const greetAction = (channel, username, botNick) => {
-  return `Welcome to Publiclab, ${username}! For a quick walkthrough, send the message: \`${botNick} help\``;
+  if(!utils.contains(greeted, username)) {
+    greeted.push(username);
+    return `Welcome to Publiclab, ${username}! For a quick walkthrough, send the message: \`${botNick} help\``;
+  }
 };
 
 module.exports = new Behavior('join', greetAction);
