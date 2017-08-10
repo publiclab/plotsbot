c018c7e0d624744b9949b22ff90b93a0e0330b7c
Ujjwal Sharma <usharma1998@gmail.com>
2017-08-10 19:27:26 +0000

    Add a utility function for executing scripts (#66)
    * Add a utility function for executing scripts
    
    * Fix linting issues
    
    * Fix bugs

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const childProcess = require('child_process');
+
 function contains(array, element) {
   return array.indexOf(element) == -1 ? false : true;
 }
@@ -6,7 +8,20 @@ function remove(array, element) {
   array.splice(array.indexOf(element), 1);
 }
 
+function exec(command) {
+  return new Promise((resolve, reject) => {
+    childProcess.exec(command, (error, stdout) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
 module.exports = {
   contains,
-  remove
+  remove,
+  exec
 };
