3b09e9f114a086a9bbd03860f52aeb962bcd541e
Ujjwal Sharma <usharma1998@gmail.com>
2017-08-15 22:22:51 +0530

    Improve test coverage

---
+++ spec/utils-spec.js
@@ -0,0 +1,23 @@
+const utils = require('../src/utils');
+
+describe('Utils Tests', () => {
+  it('should use contains function properly', () => {
+    const fruits = ['apple', 'orange', 'banana'];
+
+    expect(utils.contains(fruits, 'apple')).toBe(true);
+    expect(utils.contains(fruits, 'melon')).toBe(false);
+  });
+
+  it('should use exec function successfully', (done) => {
+    utils.exec('echo ping').then((result) => {
+      expect(result).toBe('ping\n');
+      done();
+    });
+  });
+
+  it('should fail exec function gracefully', (done) => {
+    utils.exec('exit 1').catch(() => {
+      done();
+    });
+  });
+});
