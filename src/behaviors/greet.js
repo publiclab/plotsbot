4af445e9087003436db6a18026be6d5fc6e6864c
Jeffrey Warren <jeff@unterbahn.com>
2018-03-26 17:26:05 -0400

    add Code of Conduct to greet.js

--- src/behaviors/greet.js
+++ src/behaviors/greet.js
@@ -6,7 +6,7 @@ let greeted = [];
 const greetAction = (channel, username, botNick) => {
   if(!utils.contains(greeted, username)) {
     greeted.push(username);
-    return `Welcome to Publiclab, ${username}! For a quick walkthrough, send the message: \`${botNick} help\``;
+    return `Welcome to Publiclab, ${username}! Please be aware of our Code of Conduct: https://publiclab.org/conduct. For a quick walkthrough, send the message: \`${botNick} help\``;
   }
 };
 
