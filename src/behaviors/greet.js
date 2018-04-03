f98caec870b9553caedb14ca57a844a356ad5c6b
Ujjwal Sharma <usharma1998@gmail.com>
2018-04-04 00:18:10 +0530

    Fix greet message
    Improve the greet message, as proposed by Stevie in https://github.com/publiclab/plotsbot/issues/102

--- src/behaviors/greet.js
+++ src/behaviors/greet.js
@@ -6,7 +6,7 @@ let greeted = [];
 const greetAction = (channel, username, botNick) => {
   if(!utils.contains(greeted, username)) {
     greeted.push(username);
-    return `Welcome to Publiclab, ${username}! Please be aware of our Code of Conduct: https://publiclab.org/conduct. For a quick walkthrough, send the message: \`${botNick} help\``;
+    return `Welcome to Publiclab, ${username}! Here's a link to the Code of Conduct that's in effect in this, and all other spaces of Public Lab: https://publiclab.org/conduct. For a quick walkthrough, send the message: \`${botNick} help\``;
   }
 };
 
