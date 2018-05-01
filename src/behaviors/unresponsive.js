231d7c744c1b50a592f993bf7541d9406329da54
Philip Durbin <philipdurbin@gmail.com>
2018-05-01 18:56:03 -0400

    disable "nobody's around" message #108 (#110)

--- src/behaviors/unresponsive.js
+++ src/behaviors/unresponsive.js
@@ -10,7 +10,7 @@ module.exports = ({ responseTime, offsetTime }) => {
     return new Promise((resolve, reject) => {
       currentTimeout = setTimeout(() => {
         currentTimeout = undefined;
-        resolve('Hi, looks like nobody\'s around just now. If you have a question, you can post it a https://publiclab.org/questions, or if you need help with a software issue, you can open an issue at https://github.com/publiclab/plots2/issues/new -- thanks!');
+        resolve('');
       }, responseTime);
 
       setTimeout(() => {
