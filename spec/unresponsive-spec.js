16f9e7bc9b8faba9aefcb1866079d740899e5426
Philip Durbin <philipdurbin@gmail.com>
2018-04-30 20:47:23 -0400

    disable "nobody's around" message #108

--- spec/unresponsive-spec.js
+++ spec/unresponsive-spec.js
@@ -2,14 +2,14 @@ const Behaviors = require('../src/behaviors');
 
 const responseTime = 100;
 const offsetTime = 100;
-const standardResponse = 'Hi, looks like nobody\'s around just now. If you have a question, you can post it a https://publiclab.org/questions, or if you need help with a software issue, you can open an issue at https://github.com/publiclab/plots2/issues/new -- thanks!';
+const standardResponse = '';
 
 describe('Unresponsive Behavior', () => {
   const botNick = 'testbot';
   const unresponsiveBehavior = require('../src/behaviors/unresponsive')({ responseTime, offsetTime });
   const behaviors = new Behaviors(botNick, undefined, [], [unresponsiveBehavior]);
 
-  it('should print the message if nobody responds for 10 ms', (done) => {
+  it('should not print any message if nobody responds for 10 ms', (done) => {
     behaviors.getResponse('user', botNick, 'hello').then(response => {
       expect(response).toBe(standardResponse);
       done();
