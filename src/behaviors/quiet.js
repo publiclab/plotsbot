8958f4a3dc46278d1277ed74f59096d261d7ab80
Jeffrey Warren <jeff@unterbahn.com>
2018-10-26 17:31:27 -0400

    Create quiet.js

---
+++ src/behaviors/quiet.js
@@ -0,0 +1,10 @@
+const MemoryBehavior = require('../models/memory-behavior');
+
+const quietAction = (botNick, username) => {
+  if (username.match(/\[m\]/) !== null) exempt = true; // matrix user
+  if () 
+  return `/quiet ${username}`;
+  } else {
+};
+
+module.exports = new MemoryBehavior('join', quietAction);
