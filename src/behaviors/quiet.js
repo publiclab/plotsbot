32cb8e5004ce149dfbc548fddb6d5d39f01699ca
Jeffrey Warren <jeff@unterbahn.com>
2018-11-02 10:03:35 -0400

    Update quiet.js

--- src/behaviors/quiet.js
+++ src/behaviors/quiet.js
@@ -1,10 +1,11 @@
 const MemoryBehavior = require('../models/memory-behavior');
 
+// should run only on unrecognized users
 const quietAction = (botNick, username) => {
-  if (username.match(/\[m\]/) !== null) exempt = true; // matrix user
-  if () 
-  return `/quiet ${username}`;
-  } else {
+  if (username.match(/\[m\]/) !== null) exempt = true; // exempt matrix user
+  if (exempt) {
+    return `/quiet ${username}`;
+  }
 };
 
 module.exports = new MemoryBehavior('join', quietAction);
