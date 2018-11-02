921e4f732402234a2b4ef8374c885962e95ca8fd
Jeffrey Warren <jeff@unterbahn.com>
2018-11-02 16:05:56 -0400

    Update quiet.js

--- src/behaviors/quiet.js
+++ src/behaviors/quiet.js
@@ -2,10 +2,11 @@ const MemoryBehavior = require('../models/memory-behavior');
 
 // should run only on unrecognized users
 const quietAction = (botNick, username) => {
+  client = new IrcClient(config.server, config.name, config.channels);
   var exempt = false;
   if (username.match(/\[m\]/) !== null) exempt = true; // exempt matrix user
   if (exempt !== true) {
-    client.send('quiet', '#publiclab', username); // attempting here
+    client.client.send('quiet', '#publiclab', username); // attempting here
     // return `/quiet ${username}`; // this didn't work, it just "spoke" it
   }
 };
